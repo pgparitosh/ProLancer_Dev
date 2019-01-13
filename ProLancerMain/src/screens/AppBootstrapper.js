cdfc02eb67073c84cef760dd688c75726fe4609f
pgparitosh <pgparitoshgohel@gmail.com>
2019-01-14 02:05:48 +0530

    added native base. added bottom navigation tabs. added folder structure.

---
+++ ProLancerMain/src/screens/AppBootstrapper.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import FooterTabNavigation from '../components/FooterTabNavigation';
+import { Spinner } from "native-base";
+import { Font } from "expo";
+
+export default class AppBootstrapper extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { loading: true };
+    }
+
+    async componentWillMount() {
+        await Font.loadAsync({
+            Roboto: require("native-base/Fonts/Roboto.ttf"),
+            Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf")
+        });
+        this.setState({ loading: false });
+    }
+    render() {
+        if (this.state.loading) {
+            return <Spinner color='red' />
+        }
+        return <FooterTabNavigation />
+
+    }
+}
